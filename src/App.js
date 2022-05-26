08d40536ce6abaa75b76a75699ac1518e5842639
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-05-26 14:15:35 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+import "./App.css";
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <p>Chà Mừng Bạn Đã Đến Với ReactJS</p>
+        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
+        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
