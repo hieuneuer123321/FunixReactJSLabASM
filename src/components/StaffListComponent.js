38f5889ad9aad1818a5f4478a59cb732d2a75918
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-06-01 10:56:26 +0700

    Create UI StaffListComponent

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import { Card, CardTitle } from "reactstrap";
 
 class Staffs extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="col-12 col-md-6 col-xl-4">
+        <Card>
+          <CardTitle>Nguyễn Văn A</CardTitle>
+        </Card>
+      </div>
+    );
   }
 }
 export default Staffs;
