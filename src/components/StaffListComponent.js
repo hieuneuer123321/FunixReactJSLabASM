1d1ec85d0232b8f61b15fce47a9761b6f100d3d2
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-06-01 11:03:48 +0700

    Render StaffList To View

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import { Card, CardTitle } from "reactstrap";
 
 class Staffs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
+    console.log(this.props.staffs);
+    const staffList = this.props.staffs.map((staff) => {
+      return (
+        <div className="col-12 col-md-6 col-xl-4">
+          <Card>
+            <CardTitle>{staff.name}</CardTitle>
+          </Card>
+        </div>
+      );
+    });
     return (
-      <div className="col-12 col-md-6 col-xl-4">
-        <Card>
-          <CardTitle>Nguyễn Văn A</CardTitle>
-        </Card>
+      <div className="container">
+        <div className="row">{staffList}</div>
       </div>
     );
   }
