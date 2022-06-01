dfe6ebe9aad6f61e0b1162327d906640fb1f9096
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-06-01 10:36:59 +0700

    Create Component StaffList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 ﻿import React, { Component } from "react";
-
+import Staffs from "./components/StaffListComponent";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Staffs />
+      </div>
+    );
   }
 }
 
