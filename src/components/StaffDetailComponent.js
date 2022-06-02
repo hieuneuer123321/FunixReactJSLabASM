e3151ec0383817b11e085e26537e0b5d2986fcc7
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-06-02 10:23:16 +0700

    Render StaffDetail

--- src/components/StaffDetailComponent.js
+++ src/components/StaffDetailComponent.js
@@ -1,4 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import { Card, CardTitle, CardBody, CardText } from "reactstrap";
+import dateFormat from "dateformat";
 
-class StaffDetail extends Component {}
+class StaffDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      StaffSelected: null,
+    };
+  }
+  render() {
+    console.log(this.props.StaffDetail);
+    if (this.props.StaffDetail != null) {
+      return (
+        <div className="col-12 col-md-6 col-xl-4">
+          <Card>
+            <CardBody>
+              <CardTitle>Họ Và Tên: {this.props.StaffDetail.name}</CardTitle>
+              <CardText>
+                Ngày Sinh:
+                {dateFormat(this.props.StaffDetail.doB, "dd/mm/yyyy")}
+              </CardText>
+              <CardText>
+                Ngày Vào Công Ty:
+                {dateFormat(this.props.StaffDetail.startDate, "dd/mm/yyyy")}
+              </CardText>
+              <CardText>
+                Phòng Ban: {this.props.StaffDetail.department.name}
+              </CardText>
+              <CardText>
+                Số Ngày Nghỉ Còn Lại: {this.props.StaffDetail.annualLeave}
+              </CardText>
+              <CardText>
+                Số Ngày Đã Làm Thêm: {this.props.StaffDetail.overTime}
+              </CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    } else {
+      return (
+        <div className="col-12 col-md-6 col-xl-4">
+          Bấm Vào Tên Nhân Viên Để Xem Thông Tin
+        </div>
+      );
+    }
+  }
+}
 export default StaffDetail;
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
-import { Card, CardTitle } from "reactstrap";
+import { Card, CardTitle, CardBody } from "reactstrap";
+import StaffDetail from "./StaffDetailComponent";
 
 class Staffs extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      StaffSelected: null,
+    };
+  }
+  onstaffSelect(staff) {
+    this.setState({ StaffSelected: staff });
   }
   render() {
     console.log(this.props.staffs);
@@ -12,7 +18,9 @@ class Staffs extends Component {
       return (
         <div className="col-12 col-md-6 col-xl-4">
           <Card key={staff.id} onClick={() => this.onstaffSelect(staff)}>
-            <CardTitle>{staff.name}</CardTitle>
+            <CardBody>
+              <CardTitle>{staff.name}</CardTitle>
+            </CardBody>
           </Card>
         </div>
       );
@@ -20,6 +28,9 @@ class Staffs extends Component {
     return (
       <div className="container">
         <div className="row">{staffList}</div>
+        <div className="row">
+          <StaffDetail StaffDetail={this.state.StaffSelected} />
+        </div>
       </div>
     );
   }
