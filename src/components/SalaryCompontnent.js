0ea284d32a3026c30c519d2413d44ac8c5edc150
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-06-13 17:39:50 +0700

    Update Data SalaryComponent

--- src/components/SalaryCompontnent.js
+++ src/components/SalaryCompontnent.js
@@ -4,15 +4,32 @@ import { Card, CardTitle, CardText, CardBody } from "reactstrap";
 const RenderSalary = ({ salaryList }) => {
   const styleCard = {
     margin: "20px 20px 20px 0px",
+    fontWeight: "500",
+    fontSize: "18px",
   };
+  const styleCardTitle = {
+    fontSize: "35px",
+  };
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "VND", // Ki hiệu trước số tiền
+    minimumFractionDigits: 0, // số sau dấu . cuối cùng vd 2,200.00
+  });
   return salaryList.map((salary) => (
     <div className="col-12 col-md-6 col-xl-4">
       <Card style={styleCard}>
         <CardBody>
-          <CardTitle>{salary.name}</CardTitle>
+          <CardTitle style={styleCardTitle}>{salary.name}</CardTitle>
           <CardText>Mã Nhân Viên: {salary.id}</CardText>
           <CardText>Hệ Số Lương: {salary.salaryScale}</CardText>
           <CardText>Số Ngày Làm Thêm: {salary.overTime}</CardText>
+          <CardText>
+            Lương:{" "}
+            {formatter.format(
+              salary.salaryScale * 3000000 + salary.overTime * 200000
+            )}{" "}
+            VNĐ
+          </CardText>
         </CardBody>
       </Card>
     </div>
