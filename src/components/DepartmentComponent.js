b85b5df443ada92389202ec757443d6f5eed75ff
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-06-13 16:44:07 +0700

    UI Department

--- src/components/DepartmentComponent.js
+++ src/components/DepartmentComponent.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { Card, CardTitle, CardText, CardBody } from "reactstrap";
 
-export default function DepartmentComponent(props) {
+function RenderDepartment({ departmentList }) {
   const styleCard = {
     margin: "20px 20px 20px 0px",
   };
+
+  console.log(departmentList[2]);
+  return departmentList.map((department) => (
+    <div className="col-12 col-md-6 col-xl-4">
+      <Card style={styleCard}>
+        <CardBody>
+          <CardTitle>{department.name}</CardTitle>
+          <CardText>Số Lượng Nhân Viên: {department.numberOfStaff}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  ));
+}
+
+export default function DepartmentComponent(props) {
   console.log(props);
+
   return (
     <div className="container">
       <div className="row">
-        <Card style={styleCard}>
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardText>
-              Some quick example text to build on the card title and make up the
-            </CardText>
-          </CardBody>
-        </Card>
+        <RenderDepartment departmentList={props.department} />
       </div>
     </div>
   );
