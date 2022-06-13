82ac97f2061235931fb5ffefbd5ffefa422cbb1c
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-06-13 17:21:37 +0700

    UI Salary

--- src/components/SalaryCompontnent.js
+++ src/components/SalaryCompontnent.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { Card, CardTitle, CardText, CardBody } from "reactstrap";
 
-export default function SalaryCompontnent(props) {
-  console.log(props.salary);
+const RenderSalary = ({ salaryList }) => {
   const styleCard = {
     margin: "20px 20px 20px 0px",
   };
+  return salaryList.map((salary) => (
+    <div className="col-12 col-md-6 col-xl-4">
+      <Card style={styleCard}>
+        <CardBody>
+          <CardTitle>{salary.name}</CardTitle>
+          <CardText>Mã Nhân Viên: {salary.id}</CardText>
+          <CardText>Hệ Số Lương: {salary.salaryScale}</CardText>
+          <CardText>Số Ngày Làm Thêm: {salary.overTime}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  ));
+};
+
+export default function SalaryCompontnent(props) {
+  console.log(props.salary);
+
   return (
     <div className="container">
       <div className="row">
-        <div className="col-12 col-md-6 col-xl-4">
-          <Card style={styleCard}>
-            <CardBody>
-              <CardTitle>âsa</CardTitle>
-              <CardText>Số Lượng Nhân Viên:</CardText>
-            </CardBody>
-          </Card>
-        </div>
+        <RenderSalary salaryList={props.salary} />
       </div>
     </div>
   );
