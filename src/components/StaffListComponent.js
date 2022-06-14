52351f08e05f6dfa81c8246dcdc3db34e2090dab
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-06-14 17:56:23 +0700

    Update Search Staff On Staff Name

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardImg } from "reactstrap";
 import { Link } from "react-router-dom";
 
@@ -21,6 +21,43 @@ function RenderStaff({ staff, onClick }) {
 }
 
 function Staffs(props) {
+  let [staffList, setStaff] = useState();
+
+  const Search = (staffsList) => {
+    const searchText = document.querySelector("#search").value;
+    console.log(searchText);
+    console.log(staffsList);
+    if (searchText === "") {
+      alert("Bạn chưa nhập tên nhân viên muốn tìm");
+      setStaff((staffList = <div></div>));
+    } else {
+      const searchStaffList = staffsList.filter((staff) => {
+        // toLowerCase() chuyển chuỗi thành chữ thường,
+        // .includes(searchText) kiếm tra 1 ký tự có chứ trong chuỗi hay k trả về true or false
+        return (
+          staff.name.toLowerCase().includes(searchText.toLowerCase()) === true
+        );
+      });
+      console.log(searchStaffList);
+      if (searchStaffList.length === 0) {
+        setStaff((staffList = <div></div>));
+        alert("Tên Nhân Viên Bạn Nhập K Có Trong Danh Sách");
+      } else {
+        const RenderSearchStaff = searchStaffList.map((staff) => {
+          return (
+            <div className="col-6 col-md-4 col-xl-2" key={staff.id}>
+              <RenderStaff staff={staff} onClick={props.onClick} />
+            </div>
+          );
+        });
+        setStaff((staffList = RenderSearchStaff));
+        console.log(RenderSearchStaff);
+      }
+    }
+    document.querySelector("#search").value = "";
+    return staffList;
+  };
+
   const menu = props.staffs.map((staff) => {
     return (
       <div className="col-6 col-md-4 col-xl-2" key={staff.id}>
@@ -28,16 +65,24 @@ function Staffs(props) {
       </div>
     );
   });
-
+  console.log(staffList);
   return (
     <div className="container">
       <div className="row">
         <div className="col-12">
           <h3>Nhân Viên</h3>
+          <div class="box">
+            <div class="container-4">
+              <input type="search" id="search" placeholder="Search..." />
+              <button class="icon" onClick={() => Search(props.staffs)}>
+                <i class="fa fa-search"></i>
+              </button>
+            </div>
+          </div>
           <hr />
         </div>
       </div>
-      <div className="row">{menu}</div>
+      <div className="row">{staffList ? staffList : menu}</div>
     </div>
   );
 }
