6b8d02f348ef9fa9456c87c83aac2ec5edc19e6f
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-07-03 17:07:39 +0700

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,22 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from "./reducer";
+import { createStore, combineReducers } from "redux";
+// import { Reducer, initialState } from "./reducer";
+import { Dishes } from "./dishes";
+import { Leaders } from "./leaders";
+import { Promotions } from "./promotions";
+import { Comments } from "./comment";
 
 export const ConfigureStore = () => {
+  // const store = createStore(
+  //   Reducer, // reducer
+  //   initialState // our initialState
+  // );
   const store = createStore(
-    Reducer, // reducer
-    initialState // our initialState
+    combineReducers({
+      dishes: Dishes,
+      leaders: Leaders,
+      promotions: Promotions,
+      comments: Comments,
+    })
   );
-
   return store;
 };
